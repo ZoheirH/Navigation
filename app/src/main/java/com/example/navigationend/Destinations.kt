fbbf0f5342a2ae4c9aac325783fe17d408b549b9
Z H <zh13832020@gmail.com>
2023-06-13 15:38:31 +0330

    Add Home Main Des

---
+++ app/src/main/java/com/example/navigationend/Destinations.kt
@@ -0,0 +1,15 @@
+package com.example.navigationend
+
+
+
+interface Destinations {
+    val route:String;
+}
+
+object Home : Destinations{
+    override val route="Home"
+}
+
+object Second : Destinations{
+    override val route="Second"
+}
